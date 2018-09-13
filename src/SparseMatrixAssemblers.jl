599bae7c6fe1738460b5d788ded47c67c4948409
ettersi <s.etter@warwick.ac.uk>
2018-09-13 13:19:59 +0800

    Add code

--- src/SparseMatrixAssemblers.jl
+++ src/SparseMatrixAssemblers.jl
@@ -1,5 +1,66 @@
 module SparseMatrixAssemblers
 
-# package code goes here
+using SparseArrays
+
+export SparseMatrixAssembler
+
+
+"""
+    SparseMatrixAssembler{Tv,Ti<:Integer}
+
+Convenience type for assembling sparse matrices.
+
+# Examples
+```
+julia> A = SparseMatrixAssembler();
+
+julia> A[2,2] = π;
+
+julia> Matrix(A,3,3)
+3×3 Array{Float64,2}:
+ 0.0  0.0      0.0
+ 0.0  3.14159  0.0
+ 0.0  0.0      0.0
+```
+"""
+struct SparseMatrixAssembler{Tv,Ti<:Integer}
+    i::Vector{Ti}
+    j::Vector{Ti}
+    v::Vector{Tv}
+end
+
+SparseMatrixAssembler() where {Tv} = SparseMatrixAssembler{Float64}()
+SparseMatrixAssembler{Tv}() where {Tv} = SparseMatrixAssembler{Tv,Int}()
+SparseMatrixAssembler{Tv,Ti}() where {Tv,Ti<:Integer} = SparseMatrixAssembler{Tv,Ti}(Vector{Ti}(),Vector{Ti}(),Vector{Tv}())
+
+Base.convert(::Type{SparseMatrixAssembler{Tv,Ti}}, A::SparseMatrixAssembler) where {Tv,Ti<:Integer} =
+    SparseMatrixAssembler(
+        convert(Vector{Ti},A.i),
+        convert(Vector{Ti},A.j),
+        convert(Vector{Tv},A.v)
+    )
+Base.convert(::Type{SparseMatrixAssembler{Tv}}, A::SparseMatrixAssembler) where {Tv} = convert(SparseMatrixAssembler{Tv,Int},A)
+
+
+# Overload repeat to handle scalars
+repeat(v::AbstractArray; kwargs...) = Base.repeat(v; kwargs...)
+repeat(v::Number; kwargs...) = Base.repeat([v]; kwargs...)
+
+function Base.setindex!(A::SparseMatrixAssembler, v, i,j)
+    @assert length(v) == length(i)*length(j)
+    append!(A.i, repeat(i,outer=length(j)))
+    append!(A.j, repeat(j,inner=length(i)))
+    append!(A.v, v)
+    return v
+end
+
+SparseArrays.sparse(A::SparseMatrixAssembler) = sparse(A.i,A.j,A.v)
+SparseArrays.sparse(A::SparseMatrixAssembler, m::Integer,n::Integer, combine...) = sparse(A.i,A.j,A.v,m,n, combine...)
+# The following can be ambiguous with `sparse(i,j,v,m,n)`, hence we need to explicitly write out both version as above.
+#     SparseArrays.sparse(A::SparseMatrixAssembler, args...) = sparse(A.i,A.j,A.v, args...)
+
+
+SparseArrays.SparseMatrixCSC(A::SparseMatrixAssembler, args...) = sparse(A, args...)
+Base.Matrix(A::SparseMatrixAssembler, args...) = Matrix(SparseMatrixCSC(A, args...))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,61 @@
 using SparseMatrixAssemblers
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
+using SparseArrays
+using Test
+
+@testset "Constructors" begin
+    # Mainly testing that expressions compile
+    @test typeof(SparseMatrixAssembler()) == SparseMatrixAssembler{Float64,Int}
+    @test typeof(SparseMatrixAssembler{Int32}()) == SparseMatrixAssembler{Int32,Int}
+    @test typeof(SparseMatrixAssembler{Int32,Int32}()) == SparseMatrixAssembler{Int32,Int32}
 end
 
-# write your own tests here
-@test 1 == 2
+@testset "Conversion" begin
+    # Mainly testing that expressions compile
+    @test typeof(convert(SparseMatrixAssembler, SparseMatrixAssembler())) == SparseMatrixAssembler{Float64,Int}
+    @test typeof(convert(SparseMatrixAssembler{Int32}, SparseMatrixAssembler())) == SparseMatrixAssembler{Int32,Int}
+    @test typeof(convert(SparseMatrixAssembler{Int32,Int32}, SparseMatrixAssembler())) == SparseMatrixAssembler{Int32,Int32}
+end
+
+@testset "Assignment" begin
+    @testset "Scalar" begin
+        A = SparseMatrixAssembler{Int}()
+        A[1,2] = 1
+        @test sparse(A) == [0 1]
+    end
+
+    @testset "Col vector" begin
+        A = SparseMatrixAssembler{Int}()
+        A[1:2,2] = [1,2]
+        @test sparse(A) == [0 1; 0 2]
+    end
+
+    @testset "Row vector" begin
+        A = SparseMatrixAssembler{Int}()
+        A[2,1:2] = [1,2]
+        @test sparse(A) == [0 0; 1 2]
+    end
+
+    @testset "Matrix" begin
+        A = SparseMatrixAssembler{Int}()
+        A[2:3,1:2] = [1 2; 3 4]
+        @test sparse(A) == [0 0; 1 2; 3 4]
+    end
+end
+
+@testset "Conversion to other types" begin
+    # Mainly testing that expressions compile
+    i = [1,1]
+    j = [1,1]
+    v = [1,2]
+    @test sparse(SparseMatrixAssembler(i,j,v)) == sparse(i,j,v)
+    @test sparse(SparseMatrixAssembler(i,j,v),2,2) == sparse(i,j,v,2,2)
+    @test sparse(SparseMatrixAssembler(i,j,v),2,2,*) == sparse(i,j,v,2,2, *)
+
+    @test SparseMatrixCSC(SparseMatrixAssembler(i,j,v)) == sparse(i,j,v)
+    @test SparseMatrixCSC(SparseMatrixAssembler(i,j,v),2,2) == sparse(i,j,v,2,2)
+    @test SparseMatrixCSC(SparseMatrixAssembler(i,j,v),2,2,*) == sparse(i,j,v,2,2, *)
+
+    @test Matrix(SparseMatrixAssembler(i,j,v)) == sparse(i,j,v)
+    @test Matrix(SparseMatrixAssembler(i,j,v),2,2) == sparse(i,j,v,2,2)
+    @test Matrix(SparseMatrixAssembler(i,j,v),2,2,*) == sparse(i,j,v,2,2, *)
+end
